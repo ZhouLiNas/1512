fa1ffb881efcd6bcc3b4d16fe8cdf81017cc6c95
ZhouLiNas <1641872258@qq.com>
2017-12-25 16:29:10 +0800

    my write a gub

---
+++ app.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    Selector: 'app',
+    templateUrl: './app.html',
+    styleUrls: ['./app.css']
+})
+export class MyComponent {
+    CompTitlr = 'My Component Title!';
+}
